ee4588a54ee4e4969ea56a7e1e8fac1ef96b1071
Georg Maierhofer <georg@Georgs-MacBook-Pro.local>
2020-04-22 10:20:25 +0100

    limit & sign dlp, outward normal domains

--- src/domains/param.jl
+++ src/domains/param.jl
@@ -7,5 +7,5 @@ export normal
 "Evaluate the normal of the parameterized domain at the given point."
 function normal(p::Parameterization, x)
     a = gradient(p, x)
-    SVector(-a[2], a[1])
+    SVector(-a[2], a[1])./norm(a)
 end
